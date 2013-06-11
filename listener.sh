7aa0a0b1ab90f21358b773e2b28cada8884b0806
marcker <marckfree@gmail.com>
2013-06-10 22:45:29 -0300

    Remove comentários

--- listener.sh
+++ listener.sh
@@ -1,42 +1,5 @@
 #!/bin/bash
 
-# listener.sh
-#
-# AUTHOR
-# ======
-#   Marcker
-#     - website: http://marcker.net
-#     - e-mail: marckfree@gmail.com
-#     - twitter: @marcker
-#
-# SINOPSIS
-# ========
-#  Listener is a script that serves to monitor file and apply 
-#  commands at runtime.
-#
-# USE
-# ===
-#   ./listener.sh inputfile "commmands"
-# 
-#   Basic example:
-#
-#   * Terminal 1:
-#       coffee -bcw scripts.coffee scripts.js
-#
-#     INPUT                           OUTPUT
-#   scripts.coffe | CoffeScript    | scripts.js |
-#                 | compiles to JS |
-#
-#   * Terminal 2:
-#       ./listener.sh scripts.js "yuicompressor scripts.js -o scripts.min.js"
-#
-#     INPUT                          OUTPUT
-#   scripts.js | listener.sh     | scripts.min.js
-#              | + yuicompressor |
-# 
-#   Then when saving the scripts.coffe, scripts.min.js is updated.
-# 
-
 filename=$1;commands=$2
 listener_files () {
   RED="\033[01;31m";WHITE="\033[00;00m";GREEN="\033[00;92m"
